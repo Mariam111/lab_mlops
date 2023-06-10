673e1a8858217900fecf154f2fe12a1aa9fcde8a
Mariam Gamal <mariamalboghdady@gmail.com>
2023-06-10 07:15:03 +0300

    test

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_predict():
+    response = client.post("/predict", json={"a": 1, "b": "Hi", "c":[1, 2]})
+    assert response.status_code == 200
+    assert response.json() == [2]
